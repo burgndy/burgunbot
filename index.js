df66082dcdadfa82648467a4660e74797f913447
burgndy <feathers.samuel@gmail.com>
2022-12-05 17:20:44 -0600

    part2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js")
 require("dotenv").config()
  
-
+const generateImage = require("./generateImage")
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -17,6 +18,17 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Hello World!")
     }
+})
+
+const welcomeChannelid = "1049451165487726662"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelid).send({
+        content:`<@${member.id}> Welcome faggot`,
+        files: [img]
+    })
+
 })
 
 client.login(process.env.TOKEN)
